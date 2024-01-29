338fe1531c00a86d4ab263d38803ad374f8eb2ee
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-01-29 15:04:34 +0200

    basic functionallity

---
+++ index.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const axios = require('axios');
+
+const PORT = 3000;
+const url = 'https://609a76db0f5a13001721b1e9.mockapi.io/api/v1/users';
+
+const app = new Koa();
+
+const listenFunc = () => {
+    console.log(`Server is started on ${PORT} port`);
+};
+
+const getUsers = () => axios.get(url).then((res) => res.data);
+
+const routes = {
+    "health": () => ({
+        msg: 'Server is on',
+    }),
+    "users": () => getUsers(),
+    "user": async (id) => {
+        const d = await getUsers();
+        const user = d.find(u => u.id == id);
+        return user;
+    },
+    "error" : () => ({msg: 'Route is not supported'})
+}
+app.use(async (ctx) => {
+    const [ , path ] = ctx.path.split('/');
+    const func = routes[path] || routes['error']
+    ctx.body = await func() ;
+});
+
+app.listen(PORT, listenFunc);
